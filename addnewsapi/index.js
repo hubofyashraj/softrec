49085de838f74cc7550efe1bfb5088b6485dc63b
hubofyashraj <yashraj.jangir69@gmail.com>
2024-01-12 22:34:41 +0530

    port setup

--- addnewsapi/index.js
+++ addnewsapi/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import fs from 'fs';
 import cors from 'cors';
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
